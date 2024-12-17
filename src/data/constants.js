dd6544206660c23a0db3969906280f898095a1a7
pnkjshahare <pankajsahahre.04@gmail.com>
2024-12-17 23:51:28 +0530

    update project

--- src/data/constants.js
+++ src/data/constants.js
@@ -302,13 +302,13 @@ export const projects = [
     title: "Todo Web App",
     date: "Jun 2021",
     description:
-      " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+      " A Todo Web App made with React JS, Vite, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
     image:
-      "https://camo.githubusercontent.com/84ac6ab6f378348ef28d8184062b7e9e3511a1252ae3966eaa49e8e998f732a7/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
+      "https://tichung.com/blog/2021/20200323_flask/feature.png",
     tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
     category: "web app",
-    github: "",
-    webapp: "",
+    github: "https://github.com/pnkjshahare/ToDo-List",
+    webapp: "https://to-do-list-4eq38a8pp-pankaj-shahares-projects.vercel.app/",
   },
  
   
